4cbb2410ede2efce8b170031a9200b624ea8bffe
Diego Hernandez <diegohxxiv@gmail.com>
2022-07-06 21:58:22 -0700

    Input Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,7 +19,7 @@ const Hero = ({search, setSearch}) => {
         <div className='absolute top-0 right-0 w-full h-full bg-[black]/80 grid place-items-center px-2 md:px-8'>
           <form className='w-full md:w-1/2 2xl:w-[30%] h-[50px] flex justify-center items-center bg-[white] rounded-lg' onSubmit={handleSearch}>
               <input className='w-full h-[50px] px-2 rounded-lg text-lg border-none outline-none' type="text" placeholder='Search: Houses, Apartments or Los Angeles, New York' onChange={event => setSearch(event.target.value)} value={search}/>
-              <button type='submit' className='w-[50px] h-[50px] text-[crimson] text-lg'>
+              <button type='submit' className='w-[50px] h-[50px] text-[crimson] text-lg grid place-items-center'>
                 <AiOutlineSearch/>
               </button>
           </form>
