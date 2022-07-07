eafc5596c67df2d724e3e7889a4c4a4cb750d5c4
Diego Hernandez <diegohxxiv@gmail.com>
2022-07-06 21:54:29 -0700

    Input Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,7 +16,7 @@ const Hero = ({search, setSearch}) => {
   }, [])
   return (
     <div className='relative w-full h-screen bg-[url("https://source.unsplash.com/1000x2000/?houses")] md:bg-[url("https://source.unsplash.com/2000x900/?houses")] bg-center bg-cover'>
-        <div className='absolute top-0 right-0 w-full h-full bg-[black]/80 grid place-items-center'>
+        <div className='absolute top-0 right-0 w-full h-full bg-[black]/80 grid place-items-center px-2 md:px-8'>
           <form className='w-full md:w-1/2 2xl:w-[30%] h-[50px] flex justify-center items-center bg-[white] rounded-lg' onSubmit={handleSearch}>
               <input className='w-full h-[50px] px-2 rounded-lg text-lg border-none outline-none' type="text" placeholder='Search: Houses, Apartments or Los Angeles, New York' onChange={event => setSearch(event.target.value)} value={search}/>
               <button type='submit' className='w-[50px] h-[50px] text-[crimson] text-lg'>
